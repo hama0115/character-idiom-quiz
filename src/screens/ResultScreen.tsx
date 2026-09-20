afe7bf44a43412e4391d8942091354f888c463f5
hama0115 <195052720+hama0115@users.noreply.github.com>
2026-09-21 03:07:58 +0900

    chore: 再挑戦時の文言を変更

--- src/screens/ResultScreen.tsx
+++ src/screens/ResultScreen.tsx
@@ -1,10 +1,14 @@
 type Props = {
-  correctCount: number
-  totalQuestions: number
-  onRestart: () => void
-}
+  correctCount: number;
+  totalQuestions: number;
+  onRestart: () => void;
+};
 
-export function ResultScreen({ correctCount, totalQuestions, onRestart }: Props) {
+export function ResultScreen({
+  correctCount,
+  totalQuestions,
+  onRestart,
+}: Props) {
   return (
     <div className="screen screen--centered">
       <p className="result-lead">{totalQuestions}もんちゅう</p>
@@ -12,8 +16,8 @@ export function ResultScreen({ correctCount, totalQuestions, onRestart }: Props)
         <span className="result-score-number">{correctCount}</span>もん せいかい
       </p>
       <button type="button" className="big-button" onClick={onRestart}>
-        もういっかい
+        最初からはじめる
       </button>
     </div>
-  )
+  );
 }
